e856a3aa28895dc935f696a16843b088e1ca1d51
sohan1 <shn619@gmail.com>
2016-01-25 20:38:57 +0530

    chnages

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var jsArray = [
   './bower_components/gsap/src/minified/TweenMax.min.js',
   './bower_components/ScrollMagic/scrollmagic/minified/plugins/animation.gsap.min.js',
   './bower_components/flexslider/jquery.flexslider-min.js',
+  './bower_components/wowjs/dist/wow.min.js',
   './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
   './bower_components/lodash/lodash.min.js',
 
